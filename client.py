817a570017f1c8b775beffa2e241f23f45f4099d
ElianRenteria <elianrenteriadevelopment@gmail.com>
2023-04-14 22:49:54 -0700

    added flags, many bugs

--- client.py
+++ client.py
@@ -1,37 +1,91 @@
 import socket
 import threading
-import tkinter
-import tkinter.messagebox
-import customtkinter
+import time
 
 # optional, we can use tkinter to make gui for application
 
-username = input("Enter a username: ")
+#FLAGS
+REPORT_REQUEST_FLAG = 0b00000001 #client
+REPORT_RESPONSE_FLAG = 0b00000010
+JOIN_REQUEST_FLAG = 0b00000100 #client
+JOIN_REJECT_FLAG = 0b00001000
+JOIN_ACCEPT_FLAG = 0b00010000
+NEW_USER_FLAG = 0b00100000
+QUIT_REQUEST_FLAG = 0b01000000#client
+QUIT_ACCEPT_FLAG = 0b10000000
+ATTACHMENT_FLAG = 0  #optional extra credit : If this message sends the contents of a file, this field is 1. Otherwise, it is 0.
+NUMBER = 0  #If this is a report response message where REPORT_RESPONSE_FLAG=1, this field will be set to X, where X is the number of active users, which is in range [0-3]. Otherwise, it is 0.
+USERNAME = ""  #JOIN_REQUEST_FLAG=1 or JOIN_ACCEPT_FLAG=1 or NEW_USER_FLAG=1 or QUIT_ACCEPT_FLAG=1, this field is the username else its empty
+FILENAME = ""  #extra credit optional name of file being attached
+PAYLOAD_LENGTH = 0  #length of message in characters
+PAYLOAD = ""  #contents of message
 
+flags = 0b00000000000000
+
+username = ""
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('', 18000))
 
-
+flags = 00000000000000
 def receive():
     while True:
+        print("Choose an option (1, 2, or 3):")
+        choice = input("1.Get a report of the chatroom from the server.\n2.Request to join the chatroom.\n3.Quit the program\n")
+
+        if choice == "1":
+            client.send("".encode, REPORT_REQUEST_FLAG)
+        elif choice == "2":
+            while True:
+                try:
+                    msg = client.recv(1024, flags)
+                    if flags == NEW_USER_FLAG:
+                        username = input("Enter a username: ")
+                        client.send(username.encode())
+                    elif flags == JOIN_ACCEPT_FLAG:
+                        write_thread = threading.Thread(target=write(username))
+                        write_thread.start()
+                        connectedtochat = False
+                    else:
+                        print(msg.decode())
+                except:
+                    print("You have left the server.")
+                    client.send("".encode, QUIT_REQUEST_FLAG)
+                    client.close()
+                    break
+
+        elif choice == "3":
+            print("Thank you for using our program!")
+            client.send("".encode(), QUIT_REQUEST_FLAG)
+            break
+        else:
+            print("Invalid choice")
+
+
+
+
+    """"
+    while True:
         try:
-            msg = client.recv(1024).decode()
-            if msg == "USER":
+            msg = client.recv(1024, flags)
+            if flags == NEW_USER_FLAG:
+                username = input("Enter a unique username: ")
                 client.send(username.encode())
             else:
-                print(msg)
+                print(msg.decode())
         except:
             print("You have left the server.")
+            client.send("".encode(), QUIT_REQUEST_FLAG)
             client.close()
-            break
+            break"""
 
 def write():
-    bmsg = ""
-    while bmsg != f"{username}: q":
-        msg = input("")
-        bmsg = f"{username}: {msg}"
-        if bmsg != f"{username}: q":
-            client.send(bmsg.encode())
+    PAYLOAD = ""
+    while PAYLOAD != f"{username}: q":
+        PAYLOAD = f"{username}: " + input(username+": ")
+        if PAYLOAD != f"{username}: q":
+            current_time = time.strftime("[%H:%M:%S] ")
+            client.send((current_time+PAYLOAD).encode(), flags)
+    client.send("".encode(), QUIT_REQUEST_FLAG)
     client.close()
 
 receive_thread = threading.Thread(target = receive)
--- server.py
+++ server.py
@@ -10,39 +10,44 @@ server.listen(1)
 
 clients = []
 usernames = []
+addresses = []
 chat_history = [] #need to implement this
 
-#variables we need to implement 1 if true 0 is false
-REPORT_REQUEST_FLAG = 0
-REPORT_RESPONSE_FLAG = 0
-JOIN_REQUEST_FLAG = 0
-JOIN_REJECT_FLAG = 0
-JOIN_ACCEPT_FLAG = 0
-NEW_USER_FLAG = 0
-QUIT_REQUEST_FLAG = 0
-QUIT_ACCEPT_FLAG = 0
-ATTACHMENT_FLAG = 0 # optional extra credit : If this message sends the contents of a file, this field is 1. Otherwise, it is 0.
-NUMBER = 0 # If this is a report response message where REPORT_RESPONSE_FLAG=1, this field will be set to X, where X is the number of active users, which is in range [0-3]. Otherwise, it is 0.
-USERNAME = "" # JOIN_REQUEST_FLAG=1 or JOIN_ACCEPT_FLAG=1 or NEW_USER_FLAG=1 or QUIT_ACCEPT_FLAG=1, this field is the username else its empty
-FILENAME = "" # extra credit optional name of file being attached
-PAYLOAD_LENGTH = 0 # length of message in characters
-PAYLOAD = "" # contents of message
+#FLAGS
+REPORT_REQUEST_FLAG = 0b00000001 #client
+REPORT_RESPONSE_FLAG = 0b00000010
+JOIN_REQUEST_FLAG = 0b00000100 #client
+JOIN_REJECT_FLAG = 0b00001000
+JOIN_ACCEPT_FLAG = 0b00010000
+NEW_USER_FLAG = 0b00100000
+QUIT_REQUEST_FLAG = 0b01000000#client
+QUIT_ACCEPT_FLAG = 0b10000000
+ATTACHMENT_FLAG = 0  #optional extra credit : If this message sends the contents of a file, this field is 1. Otherwise, it is 0.
+NUMBER = 0  #If this is a report response message where REPORT_RESPONSE_FLAG=1, this field will be set to X, where X is the number of active users, which is in range [0-3]. Otherwise, it is 0.
+USERNAME = ""  #JOIN_REQUEST_FLAG=1 or JOIN_ACCEPT_FLAG=1 or NEW_USER_FLAG=1 or QUIT_ACCEPT_FLAG=1, this field is the username else its empty
+FILENAME = ""  #extra credit optional name of file being attached
+PAYLOAD_LENGTH = 0  #length of message in characters
+PAYLOAD = ""  #contents of message
 
+flags = 0b00000000000000
 
-def broadcast(message):
+def broadcast(PAYLOAD):
     for client in clients:
-        client.send(message)
+        client.send(PAYLOAD)
+    chat_history.append(PAYLOAD)
 
 def handle(client):
     while True:
         try:
-            message = client.recv(1024)
-            broadcast(message)
+            PAYLOAD = client.recv(1024)
+            broadcast(PAYLOAD)
         except:
             index = clients.index(client)
             clients.remove(client)
             client.close()
             username = usernames[index]
+            address = addresses[index]
+            addresses.remove(address)
             broadcast(f"{username} has left the chat".encode())
             usernames.remove(username)
             break
@@ -50,19 +55,48 @@ def handle(client):
 def recieve():
     while True:
         client, address = server.accept()
-        client.send("Welcome to the server!".encode())
-        print(f"Connected to {address}")
+        request = client.recv(1024, flags)
+        if flags is REPORT_REQUEST_FLAG:
+            NUMBER = len(usernames)
+            PAYLOAD = ""
+            if NUMBER != 0:
+                for i in range(0,NUMBER):
+                    PAYLOAD += str(i)+ f"{usernames[i]} at {clients[i]} and port {addresses[i]}\n"
+            client.send(PAYLOAD.encode(), REPORT_RESPONSE_FLAG)
+        elif flags is JOIN_REQUEST_FLAG:
+            if len(usernames) < 3:
+                while True:
+                    client.send("".endcode(), NEW_USER_FLAG)
+                    USERNAME = client.recv(1024).decode()
+                    if USERNAME in usernames:
+                        continue
+                    else:
+                        client.send("Welcome to the Chatroom!".encode(), JOIN_ACCEPT_FLAG)
+                        print(f"Connected to {address}")
+                        usernames.append(USERNAME)
+                        clients.append(client)
+                        addresses.append(address)
+                        for message in chat_history: #might move into handle before while true
+                            client.send(message)
+                        print(f"username of client is {USERNAME}!")
+                        broadcast(f"{USERNAME} has joined the chat!".encode())
+                        thread = threading.Thread(target=handle, args=(client,))
+                        thread.start()
+            else:
+                client.send("Chatroom is Full".encode(), JOIN_REJECT_FLAG)
+        elif flags is QUIT_REQUEST_FLAG:
+            index = clients.index(client)
+            client.send("".encode(), QUIT_ACCEPT_FLAG)
+            clients.remove(client)
+            client.close()
+            USERNAME = usernames[index]
+            broadcast(f"{USERNAME} has left the chat".encode())
+            usernames.remove(USERNAME)
+            address = address[index]
+            addresses.remove(address)
+        PAYLOAD_LENGTH = len(PAYLOAD.decode())
 
-        client.send("USER".encode())
-        username = client.recv(1024).decode()
-        usernames.append(username)
-        clients.append(client)
 
-        print(f"username of client is {username}!")
-        broadcast(f"{username} has joined the chat!".encode())
-
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
 
 print("Server is ready to recieve messages")
 recieve()
